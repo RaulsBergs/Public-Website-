beb8cf8580f84cacdc8cccd6bdd504a1fcc680a1
Rauls Bergs <c00247700@itcarlow.ie>
2021-04-14 11:04:06 +0100

    Added button interactivity and restart game is possible

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const GameStates = {
 
 var gameOver = false;
 
+var playerHealth = 200;
+var enemyHealth = 200;
+
 var mouseXPos = 0;
 var mouseYPos = 0;
 
@@ -36,7 +39,6 @@ window.onload = draw();
 
 //Draws the GameScreens
 function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
 
     //if current gamestate is equal to start game draw the introduction screen
     if (GameState == GameStates.STARTGAME) {
@@ -61,10 +63,12 @@ function draw() {
 
         context.strokeRect(10, 290, 60, 200);
         context.fillStyle = "#1f7a1f";
-        context.fillRect(11, 289, 58, 200);
+        context.fillRect(11, 289, 58, playerHealth);
 
+        context.strokeRect(500, 50, 200, 30);
+        context.fillStyle = "#1f7a1f";
+        context.fillRect(500, 50, enemyHealth, 30);
     }
-
 }
 
 function getMousePosition(canvas, event) {
@@ -86,6 +90,24 @@ var instructionButtonRect = {
     posY: 382,
     posX2: 775 ,
     posY2: 438
+}
+
+var shieldButtonPosAndRad = {
+    posX: 352,
+    posY: 460,
+    radius: 37
+}
+
+var healButtonPosAndRad = {
+    posX: 598,
+    posY: 460,
+    radius: 37
+}
+
+var attackButtonPosAndRad = {
+    posX: 857,
+    posY: 460,
+    radius: 37
 }
 
 var backInstructionButtonRect = {
@@ -113,8 +135,51 @@ function mouseIntersectsPlayButton() {
     if (mouseXPos > playButtonRect.posX && mouseXPos < playButtonRect.posX2 &&
         mouseYPos > playButtonRect.posY && mouseYPos < playButtonRect.posY2) {
         GameState = GameStates.FIGHTENEMY;
+        document.getElementById("abilityLog").innerHTML = "";
+        playerHealth = 200;
+        enemyHealth = 200;
     }
 }
+
+function mouseIntersectsShieldButton() {
+    var tempRadius = ((mouseXPos - shieldButtonPosAndRad.posX) * (mouseXPos - shieldButtonPosAndRad.posX) +
+        (mouseYPos - shieldButtonPosAndRad.posY) * (mouseYPos - shieldButtonPosAndRad.posY));
+
+    tempRadius = Math.sqrt(tempRadius);
+
+    if (tempRadius < shieldButtonPosAndRad.radius) {
+        console.log("Colision!");
+        processShieldAbility();
+        processEnemyAttack();
+    }
+}
+
+function mouseIntersectsHealButton() {
+    var tempRadius = ((mouseXPos - healButtonPosAndRad.posX) * (mouseXPos - healButtonPosAndRad.posX) +
+        (mouseYPos - healButtonPosAndRad.posY) * (mouseYPos - healButtonPosAndRad.posY));
+
+    tempRadius = Math.sqrt(tempRadius);
+
+    if (tempRadius < healButtonPosAndRad.radius) {
+        console.log("Colision!");
+        processHealAbility();
+        processEnemyAttack();
+    }
+}
+
+function mouseIntersectsAttackButton() {
+    var tempRadius = ((mouseXPos - attackButtonPosAndRad.posX) * (mouseXPos - attackButtonPosAndRad.posX) +
+        (mouseYPos - attackButtonPosAndRad.posY) * (mouseYPos - attackButtonPosAndRad.posY));
+
+    tempRadius = Math.sqrt(tempRadius);
+
+    if (tempRadius < attackButtonPosAndRad.radius) {
+        console.log("Colision!");
+        processAttackAbility();
+    }
+}
+
+
 
 var myVar = setInterval(gameLoop, 33);
 
@@ -129,12 +194,20 @@ function update() {
 
 canvas.addEventListener("click", function (e) {
     getMousePosition(canvas, e);
+
     if (GameState == GameStates.STARTGAME) {
         mouseIntersectsInstructionButton();
         mouseIntersectsPlayButton();
     }
+
     if (GameState == GameStates.INSTRUCTIONSCREEN) {
         mouseIntersectsBackInstructionButton();
+    }
+
+    if (GameState == GameStates.FIGHTENEMY) {
+        mouseIntersectsShieldButton();
+        mouseIntersectsHealButton();
+        mouseIntersectsAttackButton();
     }
 });
 
@@ -142,5 +215,40 @@ function updateFrame() {
     //Calculating the x coordinate for spritesheet 
     enemySpriteSheetXPos = enemySpriteCurrentFrame * enemySpriteWidth;
     enemySpriteCurrentFrame = ++enemySpriteCurrentFrame % enemySpriteFrames;
-    //context.clearRect(enemyXpos, enemyYpos, enemySpriteWidth, enemySpriteHeight);
+}
+
+function processShieldAbility() {
+    document.getElementById("abilityLog").innerHTML = "You shielded against the enemy attack! ";
+}
+
+function processHealAbility() {
+    if (playerHealth <= 170) {
+        document.getElementById("abilityLog").innerHTML = "you have restored 30 health!";
+        playerHealth = playerHealth + 30;
+    }
+    else {
+        document.getElementById("abilityLog").innerHTML = "You can't restore any more health!";
+    }
+}
+
+function processAttackAbility() {
+    document.getElementById("abilityLog").innerHTML = "You dealt 30 damage to the enemy!";
+
+    enemyHealth = enemyHealth - 30;
+    if (enemyHealth < 0) {
+        document.getElementById("abilityLog").innerHTML = "you have defeated the enemy! Game Over";
+        GameState = GameStates.STARTGAME;
+    }
+    processEnemyAttack();
+}
+
+function processEnemyAttack() {
+    if (enemyHealth > 100) {
+        document.getElementById("abilityLog").innerHTML = "The sorcerer lit you on fire dealing 35 damage!";
+        playerHealth = playerHealth - 35;
+    } 
+    else {
+        document.getElementById("abilityLog").innerHTML = "The sorcerer regenerated 15 health!";
+        enemyHealth = enemyHealth + 15;
+    }
 }
